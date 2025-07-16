c99e232dfebb7b82c9598cb5ee266e03f04a9bdb
WalexWeb <spomeshchikov@yandex.ru>
2025-07-16 15:58:01 +0300

    add cors policy, compression for gzip and helmet

--- news-service/src/app.js
+++ news-service/src/app.js
@@ -1,28 +1,38 @@
-import express from 'express';
-import 'dotenv/config';
-import routes from './routes/index.js';
-import { connectRabbitMQ } from './config/rabbitmq.config.js';
-import { startNewsConsumers } from './services/news.service.js';
+import express from "express";
+import "dotenv/config";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import routes from "./routes/index.js";
+import { connectRabbitMQ } from "./config/rabbitmq.config.js";
+import { startNewsConsumers } from "./services/news.service.js";
 
 const app = express();
+
 const PORT = process.env.PORT || 8003;
 
 // Подключаемся к RabbitMQ и запускаем обработчики сообщений
 connectRabbitMQ()
   .then(() => startNewsConsumers())
-  .catch(err => {
-    console.error('Не удалось подключиться к RabbitMQ:', err);
+  .catch((err) => {
+    console.error("Не удалось подключиться к RabbitMQ:", err);
     process.exit(1);
   });
 
+app.use(helmet());
+
+app.use(compression());
+
+app.use(cors());
+
 app.use(express.json());
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 // Обработка ошибок
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+  res.status(500).json({ error: "Внутренняя ошибка сервера" });
 });
 
 app.listen(PORT, () => {
--- users-service/src/app.js
+++ users-service/src/app.js
@@ -1,25 +1,34 @@
-import express from 'express';
-import router from './routes/index.js';
-import { connectRabbitMQ } from './config/rabbitmq.config.js';
-import { setupMessageConsumers } from './services/message.service.js';
+import express from "express";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import router from "./routes/index.js";
+import { connectRabbitMQ } from "./config/rabbitmq.config.js";
+import { setupMessageConsumers } from "./services/message.service.js";
 
 const app = express();
 
+app.use(helmet());
+
+app.use(compression());
+
+app.use(cors());
+
 app.use(express.json());
 
-app.use('/api', router);
+app.use("/api", router);
 
 const initializeApp = async () => {
   try {
     await connectRabbitMQ();
     await setupMessageConsumers();
-    
+
     const PORT = process.env.USERS_SERVICE_PORT || 8002;
     app.listen(PORT, () => {
       console.log(`Сервер запущен на порту ${PORT}`);
     });
   } catch (error) {
-    console.error('Ошибка при инициализации приложения:', error);
+    console.error("Ошибка при инициализации приложения:", error);
     process.exit(1);
   }
 };
